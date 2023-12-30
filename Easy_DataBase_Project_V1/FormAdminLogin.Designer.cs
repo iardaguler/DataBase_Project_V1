cb468cca7b3c3130f891e70d116e83c0ce27ff7b
İsmail Arda Güler <95390026+iardaguler@users.noreply.github.com>
2023-12-30 14:45:45 +0300

    Add files via upload

---
+++ Easy_DataBase_Project_V1/FormAdminLogin.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Easy_DataBase_Project_V1
+{
+    partial class FormAdminLogin
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAdminLogin));
+            this.LabelUsername = new System.Windows.Forms.Label();
+            this.LabelPassword = new System.Windows.Forms.Label();
+            this.TextBoxPassword = new System.Windows.Forms.TextBox();
+            this.TextBoxUsername = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LabelCode = new System.Windows.Forms.Label();
+            this.TextBoxCode = new System.Windows.Forms.TextBox();
+            this.LabelRandomCode = new System.Windows.Forms.Label();
+            this.ButtonExit = new System.Windows.Forms.Button();
+            this.ButtonOkay = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LabelUsername
+            // 
+            this.LabelUsername.AutoSize = true;
+            this.LabelUsername.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LabelUsername.Location = new System.Drawing.Point(533, 100);
+            this.LabelUsername.Name = "LabelUsername";
+            this.LabelUsername.Size = new System.Drawing.Size(147, 37);
+            this.LabelUsername.TabIndex = 1;
+            this.LabelUsername.Text = "Username:";
+            // 
+            // LabelPassword
+            // 
+            this.LabelPassword.AutoSize = true;
+            this.LabelPassword.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LabelPassword.Location = new System.Drawing.Point(541, 168);
+            this.LabelPassword.Name = "LabelPassword";
+            this.LabelPassword.Size = new System.Drawing.Size(139, 37);
+            this.LabelPassword.TabIndex = 2;
+            this.LabelPassword.Text = "Password:";
+            // 
+            // TextBoxPassword
+            // 
+            this.TextBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TextBoxPassword.Location = new System.Drawing.Point(686, 176);
+            this.TextBoxPassword.Name = "TextBoxPassword";
+            this.TextBoxPassword.Size = new System.Drawing.Size(211, 29);
+            this.TextBoxPassword.TabIndex = 1;
+            this.TextBoxPassword.UseSystemPasswordChar = true;
+            this.TextBoxPassword.TextChanged += new System.EventHandler(this.TextBoxPassword_TextChanged);
+            // 
+            // TextBoxUsername
+            // 
+            this.TextBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TextBoxUsername.Location = new System.Drawing.Point(686, 108);
+            this.TextBoxUsername.Name = "TextBoxUsername";
+            this.TextBoxUsername.Size = new System.Drawing.Size(211, 29);
+            this.TextBoxUsername.TabIndex = 0;
+            this.TextBoxUsername.TextChanged += new System.EventHandler(this.TextBoxUsername_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(167, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.C)));
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // LabelCode
+            // 
+            this.LabelCode.AutoSize = true;
+            this.LabelCode.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LabelCode.Location = new System.Drawing.Point(593, 226);
+            this.LabelCode.Name = "LabelCode";
+            this.LabelCode.Size = new System.Drawing.Size(87, 37);
+            this.LabelCode.TabIndex = 11;
+            this.LabelCode.Text = "Code:";
+            // 
+            // TextBoxCode
+            // 
+            this.TextBoxCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TextBoxCode.Location = new System.Drawing.Point(686, 234);
+            this.TextBoxCode.Name = "TextBoxCode";
+            this.TextBoxCode.Size = new System.Drawing.Size(211, 29);
+            this.TextBoxCode.TabIndex = 2;
+            this.TextBoxCode.TextChanged += new System.EventHandler(this.TextBoxCode_TextChanged);
+            // 
+            // LabelRandomCode
+            // 
+            this.LabelRandomCode.AutoSize = true;
+            this.LabelRandomCode.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.LabelRandomCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LabelRandomCode.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LabelRandomCode.Location = new System.Drawing.Point(318, 289);
+            this.LabelRandomCode.Name = "LabelRandomCode";
+            this.LabelRandomCode.Size = new System.Drawing.Size(82, 39);
+            this.LabelRandomCode.TabIndex = 13;
+            this.LabelRandomCode.Text = "Code";
+            // 
+            // ButtonExit
+            // 
+            this.ButtonExit.BackColor = System.Drawing.Color.Yellow;
+            this.ButtonExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ButtonExit.BackgroundImage")));
+            this.ButtonExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ButtonExit.Location = new System.Drawing.Point(797, 287);
+            this.ButtonExit.Name = "ButtonExit";
+            this.ButtonExit.Size = new System.Drawing.Size(100, 82);
+            this.ButtonExit.TabIndex = 4;
+            this.ButtonExit.UseVisualStyleBackColor = false;
+            this.ButtonExit.Click += new System.EventHandler(this.ButtonExit_Click);
+            // 
+            // ButtonOkay
+            // 
+            this.ButtonOkay.BackColor = System.Drawing.SystemColors.Control;
+            this.ButtonOkay.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ButtonOkay.BackgroundImage")));
+            this.ButtonOkay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonOkay.Location = new System.Drawing.Point(686, 287);
+            this.ButtonOkay.Name = "ButtonOkay";
+            this.ButtonOkay.Size = new System.Drawing.Size(100, 82);
+            this.ButtonOkay.TabIndex = 3;
+            this.ButtonOkay.UseVisualStyleBackColor = false;
+            this.ButtonOkay.Click += new System.EventHandler(this.ButtonOkay_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(516, 516);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // FormAdminLogin
+            // 
+            this.AcceptButton = this.ButtonOkay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CancelButton = this.ButtonExit;
+            this.ClientSize = new System.Drawing.Size(914, 508);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.LabelRandomCode);
+            this.Controls.Add(this.TextBoxCode);
+            this.Controls.Add(this.LabelCode);
+            this.Controls.Add(this.ButtonExit);
+            this.Controls.Add(this.ButtonOkay);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TextBoxUsername);
+            this.Controls.Add(this.TextBoxPassword);
+            this.Controls.Add(this.LabelPassword);
+            this.Controls.Add(this.LabelUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormAdminLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin Login";
+            this.Load += new System.EventHandler(this.FormAdminLogin_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label LabelUsername;
+        private System.Windows.Forms.Label LabelPassword;
+        private System.Windows.Forms.TextBox TextBoxPassword;
+        private System.Windows.Forms.TextBox TextBoxUsername;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ButtonOkay;
+        private System.Windows.Forms.Button ButtonExit;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.Label LabelCode;
+        private System.Windows.Forms.TextBox TextBoxCode;
+        private System.Windows.Forms.Label LabelRandomCode;
+    }
+}
+
